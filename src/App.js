e4f20d308e88bdde45091691f37592b68edb39d6
touchableAura <evancamerondev@proton.me>
2023-08-21 18:10:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Evan's Online Blog</h1>
+      </div>
+      <div id="page-body">
+        <p>welcome to my blog</p>
+      </div>
+    </>
   );
 }
 
