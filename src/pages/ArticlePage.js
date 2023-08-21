6aea6766d632f6e88e68170fa47ddf927841056a
touchableAura <evancamerondev@proton.me>
2023-08-21 18:55:30 -0300

    changes to articles setup

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,19 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
 
 const ArticlePage = () => {
-    const params = useParams();
-    const articleId = params.articleId;
+  const params = useParams();
+  const articleId = params.articleId;
+  const article = articles.find((article) => article.name === articleId);
 
-    return(
-    <h1>this is the article page for the article with the id: {articleId}</h1>
-    );
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+import articles from "./article-content";
+import ArticlesList from '../components/ArticlesList';
+
 const ArticlesListPage = () => {
-    return(
-    <h1>this is the articles list page page</h1>
-    );
+  return (
+    <>
+      <h1>Articles</h1>
+      <ArticlesList articles={articles} />
+    </>
+  )
 };
 
 export default ArticlesListPage;
